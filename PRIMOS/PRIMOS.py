d467c05906d3bceb7eb9375fe0705846f23c64df
Jirai-Iruma <visualstudiocodenon@gmail.com>
2025-03-21 00:57:23 -0400

    while loop

--- PRIMOS/PRIMOS.py
+++ PRIMOS/PRIMOS.py
@@ -1,13 +1,18 @@
 def NUMBER_RESULT():
-    NUMBER = int(input("select a number: "))
-    if NUMBER <= 1:
-        print(f"{NUMBER} is not a prime")
-    for i in range (2,NUMBER):
-        if NUMBER % i == 0:
-            print(f"{NUMBER} is not a prime")
+    while True:
+        try:
+            NUMBER = int(input("select a number: "))
+            if NUMBER <= 1:
+                print(f"{NUMBER} is not a prime")
+            for i in range (2,NUMBER):
+                if NUMBER % i == 0:
+                    print(f"{NUMBER} is not a prime")
+                    break
+            else:
+                    print(f"{NUMBER} is a prime")
             break
-    else:
-            print(f"{NUMBER} is a prime")
+        except ValueError:
+            print("Invalid Responce")
 
 
 NUMBER_RESULT()
